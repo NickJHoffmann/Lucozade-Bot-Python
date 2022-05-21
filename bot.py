8e56b94c5ec488def1b7e16300af8fc014329624
Nick Hoffmann <59148770+NickJHoffmann@users.noreply.github.com>
2022-05-20 21:52:41 -0400

    Remove hard-coded sleep timer for dropdown selectors

--- bot.py
+++ bot.py
@@ -8,6 +8,9 @@ from time import sleep
 import json
 import random
 
+ELEMENT_EXIST_CHECK_DELAY = 0.5     # Time delay between each element existence check in seconds
+ELEMENT_EXIST_CHECK_TIMEOUT = 300   # Number of seconds to wait before timing out of element existence check
+
 with open('config.json', 'r') as config_file:
     user_info = json.load(config_file)
 
@@ -22,11 +25,35 @@ def get_element_after_loading(type, value):
         try:
             elem = driver.find_element(by=type, value=value)
         except selenium.common.exceptions.NoSuchElementException:
-            if attempts > 600:  # Time out after 5 minutes
+            if attempts > (ELEMENT_EXIST_CHECK_TIMEOUT / ELEMENT_EXIST_CHECK_DELAY):    # timeout / delay = number of attempts
+                print(f"Timed out trying to get element type={type}, value={value}")
                 raise selenium.common.exceptions.TimeoutException
-            sleep(0.5)
+            sleep(ELEMENT_EXIST_CHECK_DELAY)
         attempts += 1
     return elem
+
+
+def select_option_after_loading(parent, option_value):
+    select_option = None
+    attempts = 0
+    while not select_option:
+        try:
+            options = parent.find_elements(by=By.TAG_NAME, value="option")
+            for option in options:
+                if option.get_property("value") == option_value:
+                    select_option = option
+                    break
+            else:
+                raise selenium.common.exceptions.NoSuchElementException
+        except selenium.common.exceptions.NoSuchElementException:
+            if attempts > (ELEMENT_EXIST_CHECK_TIMEOUT / ELEMENT_EXIST_CHECK_DELAY):
+                print(f"Timed out trying to select option: {option_value}")
+                raise selenium.common.exceptions.TimeoutException
+            sleep(ELEMENT_EXIST_CHECK_DELAY)
+        attempts += 1
+
+    selector = Select(parent.find_element(by=By.TAG_NAME, value="select"))
+    selector.select_by_value(option_value)
 
 
 def fill_field(parent, field_name, value):
@@ -42,28 +69,24 @@ cookie_buttons[2].click()
 
 form_div = get_element_after_loading(By.CLASS_NAME, "infx-form-shell")
 
-
+# Enter first name and email fields
 fill_field(form_div, "firstName", user_info['first_name'])
 fill_field(form_div, "email", user_info['email'])
 fill_field(form_div, "confirmEmail", user_info['email'])
 
+# Enter phone number
 phone_section = form_div.find_element(by=By.CLASS_NAME, value="input-group-mobile")
 phone_code = Select(phone_section.find_element(by=By.CLASS_NAME, value="select"))
 phone_code.select_by_value("+44")
 fill_field(phone_section, "mobile", "7911123456")
 
+# Enter postcode
 fill_field(form_div, "postcode", "SW1A 1AA")
 
+# Select country and location dropdowns
 dropdowns = form_div.find_elements(by=By.CLASS_NAME, value="styled-select")
-
-
-dropdowns[0].click()
-sleep(2)
-country = Select(dropdowns[0].find_element(by=By.TAG_NAME, value="select"))
-country.select_by_value("NI")
-
-location = Select(dropdowns[1].find_element(by=By.TAG_NAME, value="select"))
-location.select_by_value("Tesco")
+select_option_after_loading(dropdowns[0], "NI")
+select_option_after_loading(dropdowns[1], "Tesco")
 
 form_div.find_element(by=By.NAME, value="age").click()
 form_div.find_element(by=By.NAME, value="terms").click()
