dc9bb1b5905a95ddcf13baa45860975e927f5dcf
Nick Hoffmann <59148770+NickJHoffmann@users.noreply.github.com>
2022-05-21 00:02:17 -0400

    Looping functional

--- bot.py
+++ bot.py
@@ -10,12 +10,15 @@ import random
 
 ELEMENT_EXIST_CHECK_DELAY = 0.5     # Time delay between each element existence check in seconds
 ELEMENT_EXIST_CHECK_TIMEOUT = 300   # Number of seconds to wait before timing out of element existence check
+MAX_SUBMISSIONS = 120               # Submission cap for website
+CONFIG_FILE = 'config.json'
+URL = 'https://halo.lucozade.com/'
 
-with open('config.json', 'r') as config_file:
-    user_info = json.load(config_file)
+with open(CONFIG_FILE, 'r') as file:
+    user_info = json.load(file)
 
 driver = webdriver.Chrome()
-driver.get('https://halo.lucozade.com/')
+driver.get(URL)
 action = ActionChains(driver)
 
 def get_elements_after_loading(type, value, get_all=False, parent=driver):
@@ -63,91 +66,102 @@ def fill_field(parent, field_name, value):
     elem = parent.find_element(by=By.NAME, value=field_name)
     action.move_to_element(elem).click().send_keys(value).perform()
 
-# Click "non-essential cookies" in cookie popup
-cookie_field = get_elements_after_loading(By.CLASS_NAME, "cookie-interface-masker")
-cookie_buttons = cookie_field.find_elements(by=By.CLASS_NAME, value="block-link")
-cookie_buttons[2].click()
 
 
-form_div = get_elements_after_loading(By.CLASS_NAME, "infx-form-shell")
+while user_info["num_completed"] < MAX_SUBMISSIONS:
+    # Click "non-essential cookies" in cookie popup
+    cookie_field = get_elements_after_loading(By.CLASS_NAME, "cookie-interface-masker")
+    cookie_buttons = cookie_field.find_elements(by=By.CLASS_NAME, value="block-link")
+    cookie_buttons[2].click()
 
-# Enter first name and email fields
-fill_field(form_div, "firstName", user_info['first_name'])
-fill_field(form_div, "email", user_info['email'])
-fill_field(form_div, "confirmEmail", user_info['email'])
+    form_div = get_elements_after_loading(By.CLASS_NAME, "infx-form-shell")
 
-# Enter phone number
-phone_section = form_div.find_element(by=By.CLASS_NAME, value="input-group-mobile")
-phone_code = Select(phone_section.find_element(by=By.CLASS_NAME, value="select"))
-phone_code.select_by_value("+44")
-fill_field(phone_section, "mobile", "7911123456")
+    # Enter first name and email fields
+    fill_field(form_div, "firstName", user_info['first_name'])
+    fill_field(form_div, "email", user_info['email'])
+    fill_field(form_div, "confirmEmail", user_info['email'])
 
-# Enter postcode
-fill_field(form_div, "postcode", "SW1A 1AA")
+    # Enter phone number
+    phone_section = form_div.find_element(by=By.CLASS_NAME, value="input-group-mobile")
+    phone_code = Select(phone_section.find_element(by=By.CLASS_NAME, value="select"))
+    phone_code.select_by_value("+44")
+    fill_field(phone_section, "mobile", "7911123456")
 
-# Select country and location dropdowns
-dropdowns = form_div.find_elements(by=By.CLASS_NAME, value="styled-select")
-select_option_after_loading(dropdowns[0], "NI")
-select_option_after_loading(dropdowns[1], "Tesco")
+    # Enter postcode
+    fill_field(form_div, "postcode", "SW1A 1AA")
 
-form_div.find_element(by=By.NAME, value="age").click()
-form_div.find_element(by=By.NAME, value="terms").click()
+    # Select country and location dropdowns
+    dropdowns = form_div.find_elements(by=By.CLASS_NAME, value="styled-select")
+    select_option_after_loading(dropdowns[0], "NI")
+    select_option_after_loading(dropdowns[1], "Tesco")
 
-# Try to get past captcha
-sleep((random.random() * 3) + 1.5)
-captcha = form_div.find_element(by=By.TAG_NAME, value="iframe")
-action.move_to_element(captcha).click().perform()
-sleep(1)
+    form_div.find_element(by=By.NAME, value="age").click()
+    form_div.find_element(by=By.NAME, value="terms").click()
 
-# Click "Next" button
-attempts = 0
-while attempts < (ELEMENT_EXIST_CHECK_TIMEOUT / ELEMENT_EXIST_CHECK_DELAY):
-    try:
-        btns = driver.find_elements(by=By.CLASS_NAME, value="button-text")
-        btns[1].click()
-        break
-    except selenium.common.exceptions.ElementClickInterceptedException:
-        sleep(ELEMENT_EXIST_CHECK_DELAY)
-    attempts += 1
-else:
-    print("Captcha intercepted program and was not manually overridden before timeout, exiting program")
-    exit(1)
+    # Try to get past captcha
+    sleep((random.random() * 3) + 1.5)
+    captcha = form_div.find_element(by=By.TAG_NAME, value="iframe")
+    action.move_to_element(captcha).click().perform()
+    sleep(1)
 
-
-# Page 2
-
-page_2_check = get_elements_after_loading(By.CLASS_NAME, "lz-campaign-xbox-question-one")
-page_2_dropdowns = get_elements_after_loading(By.CLASS_NAME, "styled-select", get_all=True)
-select_option_after_loading(page_2_dropdowns[0], "22")
-select_option_after_loading(page_2_dropdowns[1], "19:00")
-
-# Click "Play" button
-btns = driver.find_elements(by=By.CLASS_NAME, value="button-text")
-btns[1].click()
+    # Click "Next" button
+    attempts = 0
+    while attempts < (ELEMENT_EXIST_CHECK_TIMEOUT / ELEMENT_EXIST_CHECK_DELAY):
+        try:
+            btns = driver.find_elements(by=By.CLASS_NAME, value="button-text")
+            btns[1].click()
+            break
+        except selenium.common.exceptions.ElementClickInterceptedException:
+            sleep(ELEMENT_EXIST_CHECK_DELAY)
+        attempts += 1
+    else:
+        print("Captcha intercepted program and was not manually overridden before timeout, exiting program")
+        exit(1)
 
 
-# Page 3
+    # Page 2
 
-# Click "Click to explore" widget
-page_3_check = get_elements_after_loading(By.CLASS_NAME, "lz-campaign-galaxy-container")
-sleep(3)
-page_3_check.click()
-sleep(1)
-page_3_check.click()
+    page_2_check = get_elements_after_loading(By.CLASS_NAME, "lz-campaign-xbox-question-one")
+    page_2_dropdowns = get_elements_after_loading(By.CLASS_NAME, "styled-select", get_all=True)
+    select_option_after_loading(page_2_dropdowns[0], "22")
+    select_option_after_loading(page_2_dropdowns[1], "19:00")
 
-btn = get_elements_after_loading(By.CLASS_NAME, value="button-text")
-btn.click()
-
-# Click "Access Redemption Portal" button
-portal_check = get_elements_after_loading(By.CLASS_NAME, "lz-campaign-xbox-feedback-container")
-btn = driver.find_element(by=By.CLASS_NAME, value="button-text")
-btn.click()
+    # Click "Play" button
+    btns = driver.find_elements(by=By.CLASS_NAME, value="button-text")
+    btns[1].click()
 
 
-# Codes page
+    # Page 3
 
-code_div = get_elements_after_loading(By.CLASS_NAME, "lz-campaign-xbox-prize-wrapper")
-codes = [f"{code.text}\n" for code in get_elements_after_loading(By.CLASS_NAME, "lz-campaign-xbox-prize-code", get_all=True)]
+    # Click "Click to explore" widget
+    page_3_check = get_elements_after_loading(By.CLASS_NAME, "lz-campaign-galaxy-container")
+    sleep(2)
+    page_3_check.click()
+    sleep(1)
+    page_3_check.click()
 
-with open("codes.txt", 'w') as codes_file:
-    codes_file.writelines(codes)
+    btn = get_elements_after_loading(By.CLASS_NAME, value="button-text")
+    btn.click()
+
+    # Click "Access Redemption Portal" button
+    portal_check = get_elements_after_loading(By.CLASS_NAME, "lz-campaign-xbox-feedback-container")
+    btn = driver.find_element(by=By.CLASS_NAME, value="button-text")
+    btn.click()
+
+
+    # Codes page
+
+    code_div = get_elements_after_loading(By.CLASS_NAME, "lz-campaign-xbox-prize-wrapper")
+    codes = [f"{code.text}\n" for code in get_elements_after_loading(By.CLASS_NAME, "lz-campaign-xbox-prize-code", get_all=True)]
+
+    with open("codes.txt", 'w') as codes_file:
+        codes_file.writelines(codes)
+
+    # Increment number completed and save to config file
+    user_info["num_completed"] += 1
+    with open(CONFIG_FILE, 'w') as file:
+        json.dump(user_info, file, indent=4)
+
+    driver.delete_all_cookies()
+    sleep(2)
+    driver.refresh()
